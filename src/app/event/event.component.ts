df847a8a49039763d23848c72535a1be0618525f
amesua <ames.980519@gmail.com>
2021-05-19 18:26:58 -0500

    cambio de direcciopn forms

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -17,7 +17,7 @@ export class EventComponent implements OnInit {
   }
 
   openUrl () {
-    window.open(`https://docs.google.com/forms/d/14n1LfnWB_-h4_WkjDRxcGB005fRO9uYSUGHHs0oiBM0/prefill`,'_blank');
+    window.open(`https://forms.gle/XzoNwezGV4wMKst36`,'_blank');
   }
 
 }
